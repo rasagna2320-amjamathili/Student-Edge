901b8454749124e52ef9c5f75adea5963dece834
Rasagna2320 <rasagna.k6@gmail.com>
2025-03-28 15:52:38 +0530

    Update: Modified CDC routes to handle login and validation

--- backend/routes/cdcRoutes.js
+++ backend/routes/cdcRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import {CDC} from "../models/cdcModel.js";
-import { registerCDC } from "../controllers/cdcController.js";
+import { loginCDC,registerCDC } from "../controllers/cdcController.js";
 
 const router = express.Router();
 
+router.post("/loginCDC",loginCDC);
 router.post("/register", registerCDC);
 
 export default router;
