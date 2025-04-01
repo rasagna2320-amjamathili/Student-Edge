c70751e18913954d3011f45ccd63a570fcb6ba11
Rasagna2320 <rasagna.k6@gmail.com>
2025-04-01 11:10:06 +0530

    Updated server.js for handling profile picture uploads

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,37 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import studentRoutes from "./routes/studentRoutes.js";
+import cors from "cors";
+import {connectDB} from "./config/db.js"; 
+import studentRoutes from "./routes/studentRoutes.js"; 
 import jobRoutes from "./routes/jobRoutes.js";
 import mentorRoutes from "./routes/mentorRoutes.js";
 import cdcRoutes from "./routes/cdcRoutes.js";
-import { loginUser } from "./middlewares/authMiddleware.js";
-import bcrypt from 'bcrypt'; // Import bcrypt
-import 'dotenv/config';
 
-console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
-
-
+// Load environment variables
 dotenv.config();
+const SECRET_KEY = process.env.JWT_SECRET;
+console.log("Loaded SECRET_KEY:", SECRET_KEY);
+
+if (!SECRET_KEY) {
+    throw new Error("SECRET_KEY is missing in .env file");
+}
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));  // Adjust frontend port if needed
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-const PORT = process.env.PORT || 5000;   //Listen to .env port or by default take 5000
+const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
 
 app.get("/", (req, res) => res.send("API is running"));
 
-
-// Using the routes
-
+// Using routes
 app.use("/api/students", studentRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/cdcs", cdcRoutes);
 app.use("/api/mentors", mentorRoutes);
 
-// Start the server only once
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
