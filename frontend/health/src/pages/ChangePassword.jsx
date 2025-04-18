4498147f1ad61743157d1ffe6c69609de469d81e
AMJAMAITHILI <amjamaithili@example.com>
2025-04-19 02:06:23 +0530

    Update ChangePassword.jsx for password change functionality for editing css

--- frontend/health/src/pages/ChangePassword.jsx
+++ frontend/health/src/pages/ChangePassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-//import "./ChangePassword.css"; 
+import "./ChangePassword.css"; 
 
 const ChangePassword = () => {
   const [currentPassword, setCurrentPassword] = useState("");
