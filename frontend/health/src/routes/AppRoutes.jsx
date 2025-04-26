00958b6b2d9172ee7e7c7608d95de5f6005726b2
Rasagna2320 <rasagna.k6@gmail.com>
2025-04-26 18:49:17 +0530

    chore: Update AppRoutes.jsx)

--- frontend/health/src/routes/AppRoutes.jsx
+++ frontend/health/src/routes/AppRoutes.jsx
@@ -1,13 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import CdcLogin from "../pages/CdcLogin"; // ✅ Import CDC Login
+import CdcLogin from "../pages/CdcLogin"; 
 import Dashboard from "../pages/Dashboard";
 import ForgotPassword from "../pages/ForgotPassword"; 
 import StudentHome from "../pages/StudentHome"; 
 import ProtectedRoute from "../routes/ProtectedRoute"; 
 import StudentProfile from "../pages/StudentProfile"; 
 import ChangePassword from '../pages/ChangePassword.jsx';
+import PersonalDetails from '../pages/PersonalDetails'; 
+import ProfessionalDetails from '../pages/ProfessionalDetails'; 
+import UpdatePersonalDetails from '../pages/UpdatePersonalDetails';
+import UpdateProfessionalDetails from '../pages/UpdateProfessionalDetails';
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -15,11 +20,16 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
+
       <Route path="/cdc-login" element={<CdcLogin />} /> 
       <Route path="/student-home" element={<StudentHome />} /> 
       <Route path="/forgot-password" element={<ForgotPassword />} /> 
       <Route path="/student-profile" element={<StudentProfile />} /> 
       <Route path="/change-password" element={<ChangePassword />}/>
+      <Route path="/personal-details" element={<PersonalDetails />} /> 
+      <Route path="/professional-details" element={<ProfessionalDetails />} /> 
+      <Route path="/update-personal-details" element={<UpdatePersonalDetails />} />
+      <Route path="/update-professional-details" element={<UpdateProfessionalDetails />} />
 
  
       {/* Protected Routes */}
