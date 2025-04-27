d77e7916249b9f9e86a72bf4a4fd6e82944bcadf
AMJAMAITHILI <amjamaithili@gmail.com>
2025-04-27 15:45:35 +0530

    Updated ProtectedRoute logic for better user access control

--- frontend/health/src/routes/ProtectedRoute.jsx
+++ frontend/health/src/routes/ProtectedRoute.jsx
@@ -1,6 +1,5 @@
 import { Navigate } from "react-router-dom";
 //import { useAuth } from "../context/AuthContext"; // Ensure the correct path
-
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth(); // Assuming `AuthContext` provides a `user` object
 
